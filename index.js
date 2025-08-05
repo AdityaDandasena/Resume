e1c7cb86a7c7057292367b7d8e505049f50cfe67
Aditya Dandasena <adityadandasena120@gmail.com>
2025-08-05 19:23:02 +0530

    Resume uploaded

---
+++ index.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".navbar i");
+    const navList = document.querySelector(".navbar ul");
+
+    menuIcon.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+});
+/*resume part btn*/
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach ((btn,idx) => {
+  btn.addEventListener('click', () => {
+    const resumeDetails = document.querySelectorAll('.resume-details');
+
+    resumeBtns.forEach(btn =>
+    {
+      btn.classList.remove('active');
+    });
+    btn.classList.add('active');
+
+    resumeDetails.forEach(detail =>
+    {
+      detail.classList.remove('active');
+    });
+    resumeDetails[idx].classList.add('active');
+  });
+})
